9b30d2495c24c8dfd95925b2cb6364acf97e3519
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-11-03 21:46:23 +0100

    weather app refresh bug and saving last search bug fixed

--- script.js
+++ script.js
@@ -13,19 +13,31 @@ const checkCity = () => {
     console.log(element);
       let city = element.name;
       let temperature = Math.round(element.main.temp - 273.15);
-      let weatherDescrip = element.weather[0].description;
+      let weatherDescription = element.weather[0].description;
       let country = element.sys.country;
       let wind =  Math.round(element.wind.speed) ;
       let iconCode = element.weather[0].icon;
+
 
       if (!city){
         showErr.innerHTML = `<p>Please enter a city name</p>`
       }
         else{
+
+          const weatherData = {
+            city,
+            temperature,
+            weatherDescription,
+            iconCode,
+            country,
+            wind,
+          };
+          localStorage.setItem('weatherData', JSON.stringify(weatherData));
+
           display.innerHTML = ""
           display.innerHTML += `
           <h1 style="color:white;">${temperature}°C</h1>
-          <h3 style="color:white;">${weatherDescrip}</h3>
+          <h3 style="color:white;">${weatherDescription}</h3>
           <h3 style="color:white;">${city}</h3>
           <h3 style="color:white;">${country}</h3>
           
@@ -34,17 +46,11 @@ const checkCity = () => {
           display2.innerHTML += `
           <div style="color:white; display:flex; justify-content: space-between"><p>City</p>  <p>${city}</p></div>
           
-          <div style="color:white; display:flex; justify-content: space-between"><p>Wind</p>  <p>${wind}m/s</p></div>
           <div style="color:white; display:flex; justify-content: space-between"><p>Country</p>  <p>${country}</p></div>
           <hr style="margin-top:50px;">
           <div style="color:white; display:flex; justify-content: space-between"><p>Temperature</p>  <p>${temperature}°C</p></div>
-          <div>
-          <img src="http://openweathermap.org/img/wn/${iconCode}.png" alt="${weatherDescrip}" width="100"/>
-          </div>
+          <div style="color:white; display:flex; justify-content: space-between"><p>Wind</p>  <p>${wind}m/s</p></div>
           
-     
-          
-        
           
           `
         }
@@ -54,3 +60,25 @@ const checkCity = () => {
       console.log(error.message)
     });
 }
+
+window.addEventListener('load', () => {
+  const storedWeatherData = localStorage.getItem('weatherData');
+  if (storedWeatherData) {
+    const weatherData = JSON.parse(storedWeatherData);
+    display.innerHTML = `
+      <h1 style="color:white;">${weatherData.temperature}°C</h1>
+      <h3 style="color:white;">${weatherData.weatherDescription}</h3>
+      <h3 style="color:white;">${weatherData.city}</h3>
+      <h3 style="color:white;">${weatherData.country}</h3>
+      <img src="http://openweathermap.org/img/wn/${weatherData.iconCode}.png" alt="${weatherData.weatherDescription}" />
+    `;
+    display2.innerHTML += `
+      <div style="color:white; display:flex; justify-content: space-between"><p>City</p>  <p>${weatherData.city}</p></div>
+      <div style="color:white; display:flex; justify-content: space-between"><p>Country</p>  <p>${weatherData.country}</p></div>
+
+      <hr style="margin-top:50px;">
+      <div style="color:white; display:flex; justify-content: space-between"><p>Temperature</p>  <p>${weatherData.temperature}°C</p></div>
+      <div style="color:white; display:flex; justify-content: space-between"><p>Wind</p>  <p>${weatherData.wind}m/s</p></div>
+    `;
+  }
+});
