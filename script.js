ce6b4999b83d4af5bfdec14f5c0589a13395e443
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-11-03 16:28:49 +0100

    weather app v2.1

--- script.js
+++ script.js
@@ -10,25 +10,32 @@ const checkCity = () => {
   
   })
   .then((element)=>{
-    // console.log(element);
+    console.log(element);
       let city = element.name;
       let temperature = Math.round(element.main.temp - 273.15);
-      let weatherDescrip = element.weather[0].description
-      let ObjectWeather = {city, temperature, weatherDescrip}
+      let weatherDescrip = element.weather[0].description;
+      let country = element.sys.country;
+      let wind =  Math.round(element.wind.speed) ;
 
       if (!city){
         showErr.innerHTML = `<p>Please enter a city name</p>`
       }
         else{
-
-          ArrayWeather.push(ObjectWeather);
-          localStorage.setItem("weather", JSON.stringify(ArrayWeather));
-          JSON.parse(localStorage.getItem("weather"));
           display.innerHTML = ""
           display.innerHTML += `
-          <h1>${city}</h1>
-          <h4>${temperature}°C</h4>
-          <h4>${weatherDescrip}</h4>
+          <h1 style="color:white;">${temperature}°C</h1>
+          <h3 style="color:white;">${weatherDescrip}</h3>
+          <h3 style="color:white;">${city}</h3>
+          <h3 style="color:white;">${country}</h3>
+          
+          `
+          display2.innerHTML += `
+          
+          <br>
+          <h2 style="color:white;">${wind}Km/h Wind speed</h2>
+     
+          
+        
           
           `
         }
